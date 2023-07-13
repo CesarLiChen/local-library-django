9dca83dac8f7cd7e0f6dad6397a3f1b1203e89b2
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-13 16:14:53 -0400

    Creates the Author model

--- catalog/models.py
+++ catalog/models.py
@@ -63,3 +63,19 @@ class BookInstance(models.Model):
     def __str__(self) -> str:
         return f'{self.id} ({self.book.title})' # Python 3.6
         # return '{0} ({1})'.format(self.id, self.book.title) # For older Python versions.
+
+class Author(models.Model):
+    """ Model that represents an Author. """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    data_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+    
+    def __str__(self) -> str:
+        return f'{self.last_name}, {self.first_name}'
