624a060db4fd57ac088fe3f4ae5e6389909e6716
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-13 00:51:50 -0400

    Hooks up URL mapper

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Forwards requests with 'catalog' pattern to module 'catalog.urls'
+    path('catalog/', include('catalog.urls')),
+    # Redirects root '/' URL to catalog.
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+""" Another way of extending urlpatterns.
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
 ]
+
+# Redirects root '/' URL to catalog.
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+# Enables serving of static files during development.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+"""
