ccb2c79422ac4ecd38afa4080dccda490c996234
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-14 00:47:04 -0400

    Registers ModelAdmin classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre, Language
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+
+# Defines admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Registers admin class with associated model
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book) # Does the same as 'admin.site.register()'
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
