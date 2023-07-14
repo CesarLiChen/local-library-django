c8fb540e3c7f61e6e35f4197cd417d00182bd3b1
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-14 01:33:08 -0400

    Adds Book inline listing to Author's detailed view

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,10 +8,15 @@ from .models import Author, Book, BookInstance, Genre, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BookAdminInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
 # Defines admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookAdminInline]
 
 # Registers admin class with associated model
 admin.site.register(Author, AuthorAdmin)
@@ -23,15 +28,12 @@ class BookInstanceInline(admin.StackedInline): # admin.TabularInline for horizon
 @admin.register(Book) # Does the same as 'admin.site.register()'
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
     inlines = [BookInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id')
-
     list_filter = ('status', 'due_back')
-
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
