4dedb59600360d5264d3dc25c02fb4edcac20db3
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-17 14:46:14 -0400

    Creates Book Detail template, view class, and typo fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    # For more complex pattern matching.
+    # re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'), 
 ]
