0058b16631db739a78055478ec5af2178145a3cc
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-30 02:14:31 -0400

    Creates view for renewing book, and small typo correction

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
+import datetime
+
 from typing import Any, Dict
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Author, Book, BookInstance, Genre, Language, Secret
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
 
 # Create your views here.
 
@@ -126,3 +133,35 @@ class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
             BookInstance.objects.filter(status__exact='o')
             .order_by('due_back')
         )
+
+@login_required
+@permission_required('catalog.can_renew', raise_exception=True)
+def renew_book_librarian(request, pk):
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        
+        # Create new form instance and populate with data from 
+        # the request (binding)
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            # Process the data in form.cleaned_data as required
+            # (here we jsut write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # Redirect to new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+    
+    # If this is a GET request (or any other) create the default form.
+    else: 
+        propose_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': propose_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
