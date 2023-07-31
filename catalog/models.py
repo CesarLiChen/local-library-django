74295830f18f9cc0bada24a20dc2c5729edafb8a
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-31 00:37:10 -0400

    Adds url paths, and make migrations to autho model

--- catalog/models.py
+++ catalog/models.py
@@ -89,7 +89,7 @@ class Author(models.Model):
     """ Model that represents an Author. """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True, help_text="YYYY-MM-DD format")
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     class Meta:
