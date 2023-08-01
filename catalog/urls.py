00cf384d148723e385cd82bd6371c433aea9ba9f
Cesar Li (WSL) <cesarli619@gmail.com>
2023-08-01 16:28:01 -0400

    Creates template for Creating, and Updating book forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
     path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
     path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
 
     # For more complex pattern matching.
     # re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'), 
